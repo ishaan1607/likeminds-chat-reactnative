2f5ca02efe83a601db7af2dd89d1d8b88f657bc6
arnavr15 <arnav.ranjan@likeminds.community>
2023-12-21 17:22:33 +0530

    changed example

--- example/App.tsx
+++ example/App.tsx
@@ -25,16 +25,35 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import {
-  NavigationContainer,
-  createNavigationContainerRef,
-  StackActions,
-} from '@react-navigation/native';
-import {navigationRef} from './RootNavigation';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import ChatRoom from 'likeminds_chat_reactnative_integration/ChatSX/screens/ChatRoom';
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
 
-const Stack = createNativeStackNavigator();
+function Section({children, title}: SectionProps): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+  return (
+    <View style={styles.sectionContainer}>
+      <Text
+        style={[
+          styles.sectionTitle,
+          {
+            color: isDarkMode ? Colors.white : Colors.black,
+          },
+        ]}>
+        {title}
+      </Text>
+      <Text
+        style={[
+          styles.sectionDescription,
+          {
+            color: isDarkMode ? Colors.light : Colors.dark,
+          },
+        ]}>
+        {children}
+      </Text>
+    </View>
+  );
+}
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
@@ -44,18 +63,36 @@ function App(): React.JSX.Element {
   };
 
   return (
-    <NavigationContainer ref={navigationRef} independent={true}>
-      <Stack.Navigator>
-        <Stack.Screen
-          name={'Chatroom'}
-          component={ChatRoom}
-          initialParams={{
-            chatroomID: '3844534',
-            isInvited: false,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
