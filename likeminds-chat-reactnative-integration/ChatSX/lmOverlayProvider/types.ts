9332fc055a04092ef59d19d465f32b65fef43db2
arnavr15 <arnav.ranjan@likeminds.community>
2024-01-31 23:59:36 +0530

    resolved types

--- likeminds-chat-reactnative-integration/ChatSX/lmOverlayProvider/types.ts
+++ likeminds-chat-reactnative-integration/ChatSX/lmOverlayProvider/types.ts
@@ -1,10 +1,4 @@
 import { LMChatClient } from "@likeminds.community/chat-rn";
-import {
-  ChatBubbleStylesProps,
-  InputBoxStyles,
-  ReactionListStylesProps,
-  ThemeStyles,
-} from "../lmChatProvider/type";
 
 export interface LMOverlayProviderProps {
   myClient: LMChatClient;
