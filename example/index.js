4db2799029204c01da87046cfc9f80c7e19f1195
arnavr15 <arnav.ranjan@likeminds.community>
2024-02-07 03:43:17 +0530

    changed name and versions

--- example/index.js
+++ example/index.js
@@ -4,7 +4,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {initMyClient} from 'likeminds_chat_reactnative_integration';
+import {initMyClient} from '@likeminds.community/chat-rn-core';
 
 // proivde apiKey below to initMyClient
 const myClient = initMyClient('');
